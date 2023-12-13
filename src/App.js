9c4d0367c6b407e153a387bcb2d37725ec38fac6
[Bhavesh-0909] <133791708+Bhavesh-0909@users.noreply.github.com>
2023-12-13 18:51:00 +0530

    completed the project

--- src/App.js
+++ src/App.js
@@ -14,16 +14,16 @@ const App = () => {
 
   if(tours.length === 0){
     return (
-      <div>
-        <h2>Not Interested</h2>
-        <button onClick={()=> setTours(data)}>Refresh</button>
+      <div className="w-screen h-screen flex flex-col items-center justify-center gap-3">
+        <h2 className="text-black font-bold text-2xl">Not Interested</h2>
+        <button onClick={()=> setTours(data)} className="px-10 py-2 bg-gray-300 rounded-lg">Refresh</button>
       </div>
     );
   }
 
   return (
     <div className="flex flex-col gap-10 mx-auto p-10">
-      <h1 className="text-3xl text-red-700 border-8 px-10 py-5 border-dashed rounded-lg w-fit mx-auto">
+      <h1 className="text-4xl text-red-700 border-8 px-10 py-5 border-dashed rounded-lg w-fit mx-auto font-bold border-blue-500">
         Plan With Us
       </h1>
       <Tours tours={tours} removeTour={removeTour} ></Tours>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -14,12 +14,12 @@ function Cards({id, image, info, name, price, removeTour}){
 
     return (
 
-        <div className="w-[30%] p-2 flex flex-col shadow-[0px_0px_5px_black] rounded-lg gap-2 items-center">
+        <div className="w-[30%] min-w-[300px] h-fit p-2 flex flex-col shadow-[0px_0px_5px_black] rounded-lg gap-2 ">
             <img src={image} className="rounded-lg object-cover aspect-square"/>
             <h4 className="text-blue-500 font-bold">$ {price}</h4>
             <h3 className="font-bold text-lg">{name}</h3>
-            <p>{description}<span onClick={ReadmoreFun}>{readmore? 'Show Less' : 'Read more'}</span></p>
-            <button onClick={()=> removeTour(id)}>Not Interested</button>
+            <p>{description}<span onClick={ReadmoreFun} className="text-blue-500 cursor-pointer">{readmore? 'Show Less' : 'Read more'}</span></p>
+            <button onClick={()=> removeTour(id)} className="bg-red-300 w-full border-red-500 rounded-lg py-2 hover:bg-red-700 text-white font-bold">Not Interested</button>
         </div>
     );
 }
